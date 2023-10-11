4a29d47c016a8eda0311cb212620c03d51b4f95a
Jakub Nenczak <jnenczak@e3r7p7.42roma.it>
2023-10-11 20:13:59 +0200

    c_11

--- c_11/ex05/ft_do_op.c
+++ c_11/ex05/ft_do_op.c
@@ -6,11 +6,12 @@
 /*   By: jnenczak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 19:30:58 by jnenczak          #+#    #+#             */
-/*   Updated: 2023/10/11 19:34:15 by jnenczak         ###   ########.fr       */
+/*   Updated: 2023/10/11 20:13:44 by jnenczak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 int	ft_atoi(char *str)
 {
@@ -19,15 +20,30 @@ int	ft_atoi(char *str)
 
 	num = 0;
 	minus_count = 0;
-	while (*str < '0' && *str >= '9')
+	while (*str < '0' || *str > '9')
 	{
+		if (*str != '-' && *str != ' ' && *str != '+')
+			return (0);
 		if (*str == '-')
 			minus_count++;
 		str++;
 	}
 	while (*str >= '0' && *str <= '9')
 	{
-
+		num = num * 10 + *str - '0';
 		str++;
 	}
+	if (minus_count % 2 != 0)
+		return (-num);
+	return (num);
+}
+
+
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+		return (0);
+	printf("%d\n", ft_atoi(av[1]));
+	return (0);
 }
