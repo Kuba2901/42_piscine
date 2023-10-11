89350cc953b1f5f2134aae9474fdad9acd2de326
Jakub Nenczak <jnenczak@e3r7p7.42roma.it>
2023-10-11 20:30:44 +0200

    c_11

--- c_11/ex05/ft_do_op.c
+++ c_11/ex05/ft_do_op.c
@@ -6,7 +6,7 @@
 /*   By: jnenczak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 19:30:58 by jnenczak          #+#    #+#             */
-/*   Updated: 2023/10/11 20:19:44 by jnenczak         ###   ########.fr       */
+/*   Updated: 2023/10/11 20:30:37 by jnenczak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,74 @@ int	multiply(int val1, int val2)
 	return (val1 * val2);
 }
 
-int	ft_do_op(int val1, char *op, int val2, int (*f)(int
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			break ;
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putnbr(int nb)
+{
+	char	a;
+
+	if (nb == -2147483648)
+	{
+		write(1, "-2", 2);
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		write(1, "-", 1);
+		ft_putnbr(-nb);
+	}
+	else if (nb <= 9)
+	{
+		a = nb + 48;
+		write(1, &a, 1);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+
+void	ft_do_op(int val1, char *op, int val2)
+{
+	void (*operation[])(int, int) = {&add, &subtract, &multiply, &divide};
+	if (!ft_strcmp(op, "+"))
+		ft_putnbr(operation[0](val1, val2));
+	else if (!ft_strcmp(op, "-"))
+		ft_putnbr(operation[0](val1, val2));	else if (!ft_strcmp(op, "*"))
+		return (operation[1](val1, val2));
+	else if (!ft_strcmp(op, "/"))
+	{
+		if (!val2)
+		{
+			ft_putstr("Stop: division by zero");
+			return ;
+		}
+		return (operation[3](val1, val2));
+	}
+	else if (!ft_strcmp(op, "%"))
+	{
+		if (!val2)
+		{
+			ft_putstr("Stop: modulo by zero");
+			return ;
+		}
+		return (operation[4](val1, val2));
+	}
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
