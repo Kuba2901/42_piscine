8d96f08a185d32189ed198d77e3038d1ab0a9328
Jakub Nenczak <jnenczak@e3r5p7.42roma.it>
2023-10-12 15:50:21 +0200

    6/8 shell01

--- shell01/ex03/count_files.sh
+++ shell01/ex03/count_files.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# TODO: VERIFY
+
 find . -type f -o -type d | wc -l | tr -d ' ' 
--- shell01/ex07/r_dwssap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cat /etc/passwd | grep '^[^#]'| sed -n 'n;p' | cut -d ":" -f1 | rev | sort -r | sed -n "$FT_LINE1","$((FT_LINE2+1))"p | tr '\r\n' ', ' && echo "."
