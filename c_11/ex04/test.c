1d99a17d6c03cd592bbc0fa9c2c78133ada9c88c
Jakub Nenczak <jnenczak@e3r5p7.42roma.it>
2023-10-12 15:18:58 +0200

    changed do_op

--- c_11/ex04/test.c
+++ c_11/ex04/test.c
@@ -55,12 +55,40 @@ int	ft_is_sort_asc(int *tab, int length, int (*f)(int, int))
 
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
-	return (ft_is_sort_asc(tab, length, f) + ft_is_sort_desc(tab, length, f));
+	return (ft_is_sort_asc(tab, length, f) || ft_is_sort_desc(tab, length, f));
 }
 
-int	main(void)
+int main(void)
 {
-	int	tab[7] = {4,2,2,0,0,0};
-	printf("sorted: %d\n", ft_is_sort(tab, 5, &comp));
-	return (0);
+    int tab1[7] = {0, 1, 2, 3, 4, 5, 6};
+    printf("sorted: %d\n", ft_is_sort(tab1, 7, &comp)); // 1 (ascending)
+
+    int tab2[7] = {6, 5, 4, 3, 2, 1, 0};
+    printf("sorted: %d\n", ft_is_sort(tab2, 7, &comp)); // 1 (descending)
+
+    int tab3[7] = {0, 1, 2, 2, 3, 4, 5};
+    printf("sorted: %d\n", ft_is_sort(tab3, 7, &comp)); // 1 (ascending with equal elements)
+
+    int tab4[1] = {1};
+    printf("sorted: %d\n", ft_is_sort(tab4, 1, &comp)); // 1 (single element array)
+
+    int tab5[0] = {};
+    printf("sorted: %d\n", ft_is_sort(tab5, 0, &comp)); // 1 (empty array)
+
+    int tab6[2] = {2, 1};
+    printf("sorted: %d\n", ft_is_sort(tab6, 2, &comp)); // 1 (descending)
+
+    int tab7[5] = {0, 1, 2, 3, 2};
+    printf("sorted: %d\n", ft_is_sort(tab7, 5, &comp)); // 0 (not strictly ascending)
+
+    int tab8[5] = {3, 3, 3, 3, 3};
+    printf("sorted: %d\n", ft_is_sort(tab8, 5, &comp)); // 1 (equal elements)
+
+    int tab9[2] = {1, 1};
+    printf("sorted: %d\n", ft_is_sort(tab9, 2, &comp)); // 1 (equal elements)
+
+    int tab10[7] = {0, 0, 0, 0, 0, 0, 0};
+    printf("sorted: %d\n", ft_is_sort(tab10, 7, &comp)); // 1 (all equal elements)
+
+    return (0);
 }
