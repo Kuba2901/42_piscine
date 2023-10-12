7411aec7e3a0db9bf510b0a03a9b2c80e22ec09b
Jakub Nenczak <jnenczak@e3r5p7.42roma.it>
2023-10-12 15:05:16 +0200

    changed do_op

--- c_11/ex04/test.c
+++ c_11/ex04/test.c
@@ -10,17 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int	comp(int a, int b)
+{
+	if (a < b)
+		return (-1);
+	if (a == b)
+		return (0);
+	if (a > b)
+		return (1);
+	return (0);
+}
+
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
 	int	i;
+	int	max;
 
 	if (!tab || !length || !f)
 		return (0);
 	i = -1;
-	while (++i < length)
+	max = tab[0];
+	while (++i < length - 1)
 	{
-		if (f(tab[i], tab[i + 1]) > 0)
+		if (f(max, tab[i + 1]) > 0)
 			return (0);
+		else
+			max = tab[i];
 	}
 	return (1);
+}
+
+int	main(void)
+{
+	int	tab[7] = {0,0,2,3,0,5,6};
+	printf("sorted: %d\n", ft_is_sort(tab, 7, &comp));
+	return (0);
 }
